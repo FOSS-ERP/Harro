847e6e57a9938aead521ad8290acab12222d09d2
viralkansodiya <viral@fosserp.com>
2025-11-15 12:18:15 +0530

    fix: filter on inventory dimentation

--- harro/public/js/stock_entry.js
+++ harro/public/js/stock_entry.js
@@ -1,52 +1,35 @@
 frappe.ui.form.on("Stock Entry", {
     refresh: function (frm) {
-       
-	},
-    items_on_form_rendered(frm){
-       
-        cur_frm.fields_dict["items"].grid.get_field("bin_location").get_query = function (doc) {
+        frm.set_query("bin_location", "items", function(doc, cdt, cdn){
+            let d = locals[cdt][cdn]
+            if(!d.rack){
+                frappe.throw("Source Rack is not selected")
+            }
             return {
                 query: "harro.harro.docevents.stock_entry.get_bin_location",
-                filters: { rack: d.rack },
+                filters: { rack : d.rack },
             };
-        };
+        })
+	},
+    items_on_form_rendered(frm){
+        
     }
 })
 
 frappe.ui.form.on('Stock Entry Detail', {
-    rack(frm, cdt, cdn) {
-        let count = 0 
-        setInterval(() => {
-            if (count < 6){
-                frm.set_query("bin_location", "items", function () {
-                    let child = locals[cdt][cdn];
-                    return {
-                        filters: {
-                            rack_name: child.rack
-                        },
-                    };
-                });
-                frm.refresh_field('items');
-                count++;
-            }
-        }, 100);
-        // Refresh field so filter applies immediately     
+    refresh:function(){
+        console.log("hhhhh")
     },
-    to_rack(frm, cdt, cdn) {
-        let count = 0 
-        setInterval(() => {
-            if (count < 6){
-                frm.set_query("to_bin_location", "items", function () {
-                    let child = locals[cdt][cdn];
-                    return {
-                        filters: {
-                            rack_name: child.rack
-                        },
-                    };
-                });
-                frm.refresh_field('items');
-                count++;
+    rack:function(frm, cdt, cdn){
+        frm.set_query("bin_location", "items", function(doc, cdt, cdn){
+            let d = locals[cdt][cdn]
+            if(!d.rack){
+                frappe.throw("Source Rack is not selected")
             }
-        }, 100);
-    },
+            return {
+                query: "harro.harro.docevents.stock_entry.get_bin_location",
+                filters: { rack : d.rack },
+            };
+        })
+    }
 });
