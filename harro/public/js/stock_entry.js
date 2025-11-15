1ed535bb211d940c79150ad9f3593703e4ed7ba1
viralkansodiya <viral@fosserp.com>
2025-11-15 12:20:07 +0530

    fix: filter on inventory dimentation

--- harro/public/js/stock_entry.js
+++ harro/public/js/stock_entry.js
@@ -10,6 +10,16 @@ frappe.ui.form.on("Stock Entry", {
                 filters: { rack : d.rack },
             };
         })
+        frm.set_query("to_bin_location", "items", function(doc, cdt, cdn){
+            let d = locals[cdt][cdn]
+            if(!d.rack){
+                frappe.throw("Target Rack is not selected")
+            }
+            return {
+                query: "harro.harro.docevents.stock_entry.get_bin_location",
+                filters: { rack : d.to_rack },
+            };
+        })
 	},
     items_on_form_rendered(frm){
         
@@ -29,6 +39,18 @@ frappe.ui.form.on('Stock Entry Detail', {
             return {
                 query: "harro.harro.docevents.stock_entry.get_bin_location",
                 filters: { rack : d.rack },
+            };
+        })
+    },
+    to_rack:function(frm, cdt, cdn){
+        frm.set_query("to_bin_location", "items", function(doc, cdt, cdn){
+            let d = locals[cdt][cdn]
+            if(!d.rack){
+                frappe.throw("Target Rack is not selected")
+            }
+            return {
+                query: "harro.harro.docevents.stock_entry.get_bin_location",
+                filters: { rack : d.to_rack },
             };
         })
     }
